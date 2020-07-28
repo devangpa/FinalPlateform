de36c98ac55e6bece68646dd816dc58139583dbc
devangp <devangp@microlink.co.in>
2020-07-28 16:07:27 +0530

    vorkInsta Authorization deplyeed

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {logoutUser} from '../../actions/authActions';
+import Footer from './Footer';
+import {use} from 'passport';
 
 class Dashboard extends Component {
   onLogoutClick = (e) => {
@@ -11,34 +13,87 @@ class Dashboard extends Component {
 
   render() {
     const {user} = this.props.auth;
-    console.log(user);
+    console.log(user.name);
 
     return (
-      <div style={{height: '75vh'}} className='container valign-wrapper'>
-        <div className='row'>
-          <div className='landing-copy col s12 center-align'>
-            <h4>
-              <b>Hey there,</b> {user.name.split(' ')[0]}
-              <p className='flow-text grey-text text-darken-1'>
-                You are logged into a full-stack{' '}
-                <span style={{fontFamily: 'monospace'}}>MERN</span> app 👏
-              </p>
-            </h4>
-            <button
+      <nav>
+        <div class='navbar-fixed' style={{backgroundColor: '#2979FF'}}>
+          <a href='#' class='brand-logo' style={{margin: '10px 10px'}}>
+            <h6
               style={{
-                width: '150px',
-                borderRadius: '3px',
-                letterSpacing: '1.5px',
-                marginTop: '1rem',
+                fontFamily: 'monospace',
+                letterSpacing: '2px',
+                userSelect: 'none',
               }}
-              onClick={this.onLogoutClick}
-              className='btn btn-large waves-effect waves-light hoverable blue accent-3'
             >
-              Logout
-            </button>
+              vorkInsta
+            </h6>
+          </a>
+          <ul class=' right hide-on-med-and-down'>
+            <li style={{marginRight: '5px'}}>
+              <a>
+                <i class='Large material-icons'>home</i>{' '}
+              </a>
+            </li>
+
+            <li style={{marginRight: '5px'}}>
+              <a>
+                <i class='Large material-icons'>group</i>{' '}
+              </a>
+            </li>
+
+            <li style={{marginRight: '5px'}}>
+              <a>
+                <i class='Large material-icons'>work</i>{' '}
+              </a>
+            </li>
+            <li style={{marginRight: '5px'}}>
+              <a>
+                <i class='Large material-icons'>chat</i>{' '}
+              </a>
+            </li>
+            <li style={{marginRight: '200px'}}>
+              <a>
+                <i class='Large material-icons'>notifications</i>{' '}
+              </a>
+            </li>
+
+            <li>
+              <a href='#' onClick={this.onLogoutClick}>
+                <i class='Large material-icons'>exit_to_app</i>{' '}
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div style={{height: '75vh'}} className='container valign-wrapper'>
+          <div className='row'>
+            <div className='landing-copy col s12 center-align'>
+              <h4>
+                <h6>Hey There{user.name} </h6>
+                <p className='flow-text grey-text text-darken-1'>
+                  Welcome To vorkInsta We will come soon With new
+                  <span style={{fontFamily: 'monospace'}}>Vorkinsta</span> app
+                  👏
+                </p>
+              </h4>
+              <button
+                style={{
+                  width: '150px',
+                  borderRadius: '3px',
+                  letterSpacing: '1.5px',
+                  marginTop: '1rem',
+                }}
+                onClick={this.onLogoutClick}
+                className='btn btn-large waves-effect waves-light hoverable blue accent-3'
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+        <Footer />
+      </nav>
     );
   }
 }
---
+++ client/src/components/dashboard/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const Footer = () => {
+  return (
+    <div>
+      <footer class='page-footer' style={{backgroundColor: '#2979FF'}}>
+        <div class='footer-copyright'>
+          <div class='container'>
+            ©2020 Copyright Reserve With vorkInsta
+            <a class='grey-text text-lighten-4 right' href='#!'>
+              More Links
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
